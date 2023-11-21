23957ca48da5591063b87baa8544d3ff7d4d5da3
AbubakarCinnova <amahboob@cinnova.com>
2023-11-21 11:01:00 +0500

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <main>
         <div class="video-banner">
             <div style="padding:42.19% 0 0 0;position:relative;"><iframe
-                    src="https://player.vimeo.com/video/861833688?h=cb0f8ccbf6&autoplay=1&loop=1&title=0&byline=0&portrait=0"
+                    src="https://player.vimeo.com/video/861833688?h=cb0f8ccbf6&autoplay=1&loop=1&title=0&byline=0&portrait=0&controls=0"
                     style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0"
                     allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div>
             <script src="https://player.vimeo.com/api/player.js"></script>
