e6beea8d8aea3cb8af5f263fb5659d9023bb3547
AbubakarCinnova <amahboob@cinnova.com>
2023-11-21 11:10:09 +0500

    Update index.php

--- index.php
+++ index.php
@@ -9,12 +9,15 @@
 
     <main>
         <div class="video-banner">
-            <div style="padding:42.19% 0 0 0;position:relative;"><iframe
-                    src="https://player.vimeo.com/video/861833688?h=cb0f8ccbf6&autoplay=1&loop=1&title=0&byline=0&portrait=0&controls=0"
+            <div style="padding:42.19% 0 0 0;position:relative;">
+                <iframe
+                    src="https://player.vimeo.com/video/861833688?h=cb0f8ccbf6&autoplay=1&muted=1&loop=1&title=0&byline=0&portrait=0&controls=0"
                     style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0"
-                    allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div>
+                    allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+            </div>
             <script src="https://player.vimeo.com/api/player.js"></script>
         </div>
+
         <!--  <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" data-interval="7500">
 
             <div class="carousel-inner" role="listbox">
