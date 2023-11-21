e16ec39198a214510f3f4a9e6ee8b60e247ac840
AbubakarCinnova <amahboob@cinnova.com>
2023-11-21 16:03:08 +0500

    air-cond updated

---
+++ air-conditioners.php
@@ -0,0 +1,195 @@
+<?php include_once('header.php') ?>
+<title>Paitson Bros Heating and Air Conditioning | Air Conditioners </title>
+<meta name="description"
+    content=" Paitson Bros. Heating and Air Conditioning specializes in the installation of state-of-the-art, cost-effective, energy-efficient, and high-performance air conditioners for both residential and commercial properties.">
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+                                <img src="<?php echo $base_url ?>/img/air-cond.jpg" alt="Paitson ac banner"
+                                    class="full-width-img">
+
+                                <br>
+                                <br>
+
+                                <h2>Is it about time to consider a replacement for your air conditioner?</h2>
+                                <p>When the heat and humidity of summer hit Indiana, having an air conditioning system
+                                    that
+                                    meets your expectations is crucial. If your existing central air system is
+                                    struggling to
+                                    keep up, it might be the perfect moment to invest in an upgrade. Paitson Bros.
+                                    Heating
+                                    and Air Conditioning specializes in the installation of state-of-the-art,
+                                    cost-effective, energy-efficient, and high-performance air conditioners and cooling
+                                    systems for both residential and commercial properties.</p>
+
+
+                                <p>Discover the unparalleled impact of the Wabash Valley's most seasoned HVAC
+                                    installation
+                                    team on your home's comfort and safety. Uncover the significance of our position as
+                                    one
+                                    of the Valley's last full-service residential sheet metal shops. Explore the install
+                                    best practices we unwaveringly adhere to, setting us apart from the competition. We
+                                    don't just compete; we redefine excellence in a class of our own. Your home deserves
+                                    nothing less.</p>
+
+
+
+
+
+                                <h2>What is the significance of the SEER rating in an AC system?</h2>
+
+                                <p>SEER, which stands for "Seasonal Energy Efficiency Ratio," quantifies the cooling
+                                    output
+                                    of an air conditioning system throughout a standard cooling season, divided by the
+                                    energy consumed in watt-hours.</p>
+
+
+
+                                <h2>Variable-Capacity AC Compressor</h2>
+                                <p>The Variable-capacity compressor also called Variable Speed is currently our most
+                                    efficient and quietest option. It can operate at various speeds depending on the
+                                    cooling
+                                    requirements, ranging from 35-100%. Compared to two-stage systems, it offers a wider
+                                    range of speeds, enabling the air conditioning to maintain lower speeds for longer
+                                    durations while providing the option for higher speeds when the outdoor temperature
+                                    rises. As a result, variable-speed AC systems run continuously without frequent
+                                    on/off
+                                    cycles and only at the required speed to keep you comfortable, making them the most
+                                    energy-efficient among the three types of AC systems.</p>
+
+
+
+                                <h2>Two-Stage / Multi Stage AC Compressor</h2>
+
+                                <p>The two-stage AC compressor offers dual functionality by operating at high and low
+                                    levels. It runs at a lower capacity initially (70%) on milder days and increases to
+                                    100%
+                                    if needed. This results in more consistent indoor temperatures as it operates for
+                                    longer
+                                    periods. Compared to a single-stage compressor, the two-stage version is more
+                                    efficient
+                                    and durable, as it doesn't cycle on and off as frequently. Additionally, it
+                                    effectively
+                                    removes more humidity due to prolonged operation at the lower setting.</p>
+
+
+
+                                <h2>Single-Stage AC Compressor</h2>
+
+                                <p>A single-stage AC compressor functions at a constant full-cold speed, with the
+                                    thermostat controlling the home's temperature by turning the AC system and
+                                    compressor on
+                                    and off. In Indiana, most homes with central air conditioning use a single-stage
+                                    compressor, which has been the standard for several decades. The latest single-stage
+                                    models feature cutting-edge technology, making them considerably more
+                                    energy-efficient
+                                    than their older counterparts, which had much lower SEER ratings.</p>
+
+
+
+                                <h2>Tailored Heating Solutions</h2>
+
+                                <p> With a century of business experience, Paitson Bros. Heating and Air Conditioning
+                                    specialize in personalized home comfort solutions. Taking into consideration
+                                    variables
+                                    like home size, design, climate, insulation, and occupants' behavior, they identify
+                                    the
+                                    most cost-effective and energy-efficient options. Their recommendations adhere to
+                                    stringent criteria, and they offer comprehensive explanations, suggestions, and load
+                                    calculations to ensure the correct sizing of equipment for each home. Committed to
+                                    delivering top-notch customer service, their aim is to establish enduring
+                                    relationships
+                                    with customers.</p>
+
+                                <h1>Our Most Popular Lennox Air Conditioners</h1>
+
+                                <h2>Lennox Model SL28XCV</h2>
+                                <ul>
+                                    <li>The most precise and efficient air conditioner available</li>
+                                    <li>Stages of Cooling: Variable-Capacity Operation</li>
+                                    <li>Up to 28 SEER</li>
+                                    <li>SEER2 Up to 25.8</li>
+                                    <li>ENERGY STAR®</li>
+                                    <li>Sound Level As low as 59 dBA</li>
+                                    <li>Cabinet PermaGuard Cabinet and SmartHinge Louver Design</li>
+                                    <li>R-410A refrigerant</li>
+                                    <li>Tax Credit Eligible</li>
+                                    <li>iComfort-enabled technology</li>
+                                    <li>10-Year Limited Warranty on compressor and covered components.</li>
+                                </ul>
+
+                                <h2>LENNOX MODEL EL18XCV</h2>
+                                <ul>
+                                    <li>Up to 18 SEER</li>
+                                    <li>SEER2 up to 18.8</li>
+                                    <li>Stages of Cooling Variable-Capacity Operation</li>
+                                    <li>ENERGY STAR®</li>
+                                    <li>Sound Level As low as 72 dBA</li>
+                                    <li>PermaGuard Cabinet and SmartHinge Louver Design</li>
+                                    <li>R-410A refrigerant</li>
+                                    <li>Tax Credit Eligible</li>
+                                    <li>iComfort-enabled technology</li>
+                                    <li>Elite models come with a 10-Year Limited Warranty on compressors and a 5-Year
+                                        Limited Warranty on covered components. A 10-Year Extended Limited Warranty is
+                                        available with product registration.</li>
+                                </ul>
+
+                                <h2>LENNOX MODEL ML18XC2</h2>
+                                <ul>
+                                    <li>Mid-efficiency, two-stage air conditioner</li>
+                                    <li>18.00 SEER and 17.8 SEER2</li>
+                                    <li>Stages of Cooling Two-Stage Operation</li>
+                                    <li>ENERGY STAR®</li>
+                                    <li>Sound Level As low as 75 dBA</li>
+                                    <li>PermaGuard Cabinet</li>
+                                    <li>R-410A refrigerant</li>
+                                    <li>On-board Diagnostics</li>
+                                    <li>5-Year Limited Warranty on covered components including compressor. This product
+                                        may be eligible for valuable 10-Year Limited Extended Warranty coverage with
+                                        product registration.</li>
+                                </ul>
+
+                                <h2>LENNOX MODEL EL16XC1</h2>
+                                <ul>
+                                    <li>High-Efficiency Air Conditioner</li>
+                                    <li>17 SEER</li>
+                                    <li>Stages of Cooling Single Stage</li>
+                                    <li>ENERGY STAR®</li>
+                                    <li>Sound Level As low as 71 dBA</li>
+                                    <li>PermaGuard Cabinet and SmartHinge Louver Design</li>
+                                    <li>R-410A refrigerant</li>
+                                    <li>Tax Credit Eligible</li>
+                                    <li>Elite models come with a 10-Year Limited Warranty on compressors and a 5-Year
+                                        Limited Warranty on covered components. A 10-Year Extended Limited Warranty is
+                                        available with product registration.</li>
+                                </ul>
+
+                                <p><strong>Contact Us for a Free in-Home Estimate For a New Home Comfort System</strong>
+                                </p>
+
+
+                                <br>
+
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Request
+                                    Quote</a>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
