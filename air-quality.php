1d79571114ef5c5924d27b9d6ecff879b65bfec3
AbubakarCinnova <amahboob@cinnova.com>
2023-12-07 17:57:12 +0500

    air quality updated

---
+++ air-quality.php
@@ -0,0 +1,190 @@
+<?php include_once('header.php') ?>
+
+<title>Paitson Bros. Heating and Air Conditioning | Air Quality</title>
+
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-md-12">
+
+                                <h2>Indoor Air Quality</h2>
+                                <p>Did you know the U.S. EPA has estimated that indoor air levels of pollutants may
+                                    be 2 to 5 times - and occasionally more than 100 times - higher than outdoor air
+                                    pollutant levels. Indoor air pollutants have been ranked among the top five
+                                    environmental risks to public health. The problems they cause can be subtle and
+                                    do not always produce easily recognized or immediate impacts on health. (source
+                                    U.S. E.P.A.) The average adult at rest inhales and exhales around 7 or 8 liters
+                                    of air per minute. That totals to be around 11,000 liters of air (388 cubic
+                                    feet) in a day. So it's important that air you and your family breathe in your
+                                    home be clear of pollutants.</p><br>
+
+                                <h2>Paitson Bros. Heating and Air Conditioning Indoor Air Quality Specialists</h2>
+                                <p>Paitson Bros. Heating and Air Conditioning's, whole home indoor air quality approach
+                                    not only
+                                    increases the quality of the air within your home, but it also makes a
+                                    substantial reduction in your homes energy use, which in turn saves you money on
+                                    your energy bills.</p><br>
+                                <p>Paitson Bros. Heating and Air Conditioning are experts at providing air filtration
+                                    products that
+                                    work along with your central heating and central air conditioning system. The
+                                    solutions below used by themselves are effective, but in combination, provide a
+                                    maximum solution for those suffering from asthma, allergies or other breathing
+                                    issues.</p><br>
+
+                                <p>Today's homes are better insulated than ever. While this is great for energy
+                                    savings, it also means that millions of airborne particles are trapped indoors
+                                    where they continue to circulate throughout your home, and the air your family
+                                    breathes. The Lennox PureAir system whole home Air purifiers and filter systems,
+                                    work with your heating and cooling system to provide a thorough, whole house
+                                    solution to remove these particles and dramatically improve the quality of your
+                                    indoor air. Besides providing excellent filtration unmatched by any
+                                    manufacturer, these home air quality products can also help prolong the life of
+                                    your heating and cooling system by keeping it free from dirt and dust buildup.
+                                </p>
+
+                                <h2>Air Purifiers and Filters</h2>
+                                <img src="<?php echo $base_url ?>/img/pure air.jpg" width="25%"><br>
+                                <p>Today's homes are better insulated than ever. While this is great for energy
+                                    savings, it also means that millions of airborne particles are trapped indoors
+                                    where they continue to circulate throughout your home, and the air your family
+                                    breathes. The Lennox PureAir system whole home Air purifiers and filter systems,
+                                    work with your heating and cooling system to provide a thorough, whole house
+                                    solution to remove these particles and dramatically improve the quality of your
+                                    indoor air. Besides providing excellent filtration unmatched by any
+                                    manufacturer, these home air quality products can also help prolong the life of
+                                    your heating and cooling system by keeping it free from dirt and dust buildup.
+                                </p>
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" target="_self"
+                                    class="btn btn-warning">Request
+                                    Estimate</a><br><br>
+
+                                <p>Your best defense against poor indoor air quality, is the the PureAir system it
+                                    uses exclusive UVA-light technology to attack all three classes of indoor air
+                                    contaminants-particles, mold/mildew/bacteria and odors/chemical vapors. It not
+                                    only reduces, but also destroys ozone, a known lung irritant.</p><br>
+
+
+
+
+                                <h2>PureAir system</h2>
+                                <ul>
+                                    <li>Only single indoor air quality system to attack all three classes of indoor
+                                        air contaminants:</li>
+                                    <li>Small, breathable particles such as dust, dirt, pollen and allergens.</li>
+                                    <li>Airborne mold spores, bacteria and viruses.</li>
+                                    <li>Odors and chemical vapors.</li>
+                                    <li>Hospital inpatient care/general surgery level filtration, whole-home
+                                        purification works with your existing heating and cooling system.</li>
+                                    <li>5-Year Limited Warranty on covered components.</li>
+                                    <li><a href="<?php echo $base_url ?>/pdf/Lennox_PureAir_System brochure.pdf">View
+                                            Brochure</a></li>
+                                </ul><br><br>
+
+                                <iframe width="560" height="315" src="https://www.youtube.com/embed/r10Xu-QherI?rel=0"
+                                    frameborder="0" allow="autoplay; encrypted-media" allowfullscreen=""></iframe><br>
+
+
+                                <h2>UV Germicidal Lights</h2>
+                                <img src="<?php echo $base_url ?>/img/lennox uv lamp.png" width="25%"><br>
+                                <p>Germicidal lights use intense rays of ultraviolet light to control and kill
+                                    contaminants like viruses, bacteria, mold and yeast. Installed inside your HVAC
+                                    duct system. Lennox Healthy Climate UV Lamp attacks and kills mold and bacteria
+                                    that can grow in moist heating and cooling systems, then circulate through your
+                                    home. Silent operation enhances your family's comfort and health. Improves the
+                                    air in your entire home.</p>
+                                <p>Call us today to have one installed in your central air system.</p><br>
+
+                                <h2>Lennox Healthy Climate UV Lamp</h2>
+                                <ul>
+                                    <li>Powerful ultraviolet energy helps eliminate biological contaminants like
+                                        mold and bacteria.</li>
+                                    <li>Improves air quality and protects equipment by sterilizing surfaces.</li>
+                                    <li>Dual-lamp or single-lamp option.</li>
+                                    <li>Available in 2,000 microwatts (UV-2000) or 1,496 microwatts (UV-1000).</li>
+                                    <li>UL listed.</li>
+                                    <li>5-Year Limited Warranty on covered components.</li>
+                                    <li><a href="<?php echo $base_url ?>/pdf/Lennox_Germicidal_Lamps brochure.pdf">View
+                                            Brochure</a></li>
+                                </ul><br><br>
+
+
+
+                                <h2>Whole House Humidifiers</h2>
+                                <img src="<?php echo $base_url ?>/img/lennox humidfier.png" width="25%"><br>
+                                <p>By adding humidity to heated, dry air, the air feels warmer, helps reduce dry
+                                    skin and static shock plus you can lower the thermostat and still be
+                                    comfortable, while saving money. The Healthy Climate Power Humidifier attaches
+                                    to your furnace or air handler and adds moisture to air moving through the duct
+                                    system. A built-in fan allows the unit to circulate humidified air even when
+                                    your heating system is not in operation.</p>
+
+                                <h2>Healthy Climate Power Humidifier </h2>
+                                <ul>
+                                    <li>Maintains optimal comfort during heating seasons by minimizing problems
+                                        associated with dry air.</li>
+                                    <li>Creates a healthier home and lessens skin irritations, sore throats and
+                                        respiratory problems.</li>
+                                    <li>Provides an easier-to-use option to portable units-no need to constantly
+                                        fill and clean units.</li>
+                                    <li>Built-in fan helps circulate humidified air.</li>
+                                    <li>Available with automatic control to keep humidity precisely where you need
+                                        it.</li>
+                                    <li>5-Year Limited Warranty on covered components.</li>
+                                    <li>Automatic Refilling.</li>
+                                    <li><a href="<?php echo $base_url ?>/pdf/Lennox_WB2-WP2_Humidifiers brochurt.pdf">View
+                                            Brochure</a></li>
+                                </ul><br><br>
+
+                                <h2>Ventilators</h2>
+                                <img src="<?php echo $base_url ?>/img/lennox-ventilator.png" width="25%"><br>
+                                <p>New homes and some older homes can be tightly built to save energy, but this can
+                                    often times have a negative impact on homes without proper ventilation. An
+                                    airtight home, provides no way to refresh the indoor environment. An ideal
+                                    solution for tightly sealed homes is a Health Climate Energy Recovery Ventilator
+                                    (ERV). The ERV system exchanges stale air from inside with fresher outdoor air.
+                                    To ensure absolute comfort, the system captures heat from contaminated air
+                                    before it is moved outdoors.</p>
+
+                                <h2>Healthy Climate Energy Recovery Ventilator</h2>
+                                <ul>
+                                    <li>Provides cleaner, fresher indoor air.</li>
+                                    <li>Designed to provide comfort in colder climates.</li>
+                                    <li>Transfers heat from indoor air before it is discharged.</li>
+                                    <li>Will not restrict airflow for proper filtration.</li>
+                                    <li>Ideal for new homes which are tightly sealed or remodeled homes with energy
+                                        efficiency upgrades.</li>
+                                    <li>All units include a Healthy Climate wall control.</li>
+                                    <li>5-Year Limited Warranty on covered components.</li>
+                                    <li><a href="<?php echo $base_url ?>/pdf/Lennox_HRV-ERV_Ventilators brochure.pdf">View
+                                            Brochure</a></li>
+                                </ul><br><br>
+
+                                <a href="<?php echo $base_url ?>/contact-us.php" target="_self"
+                                    class="btn btn-warning">Request Quote
+                                </a><br>
+                                <br>
+
+
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
