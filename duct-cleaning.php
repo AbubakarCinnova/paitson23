d05922dbd33b65825011ce3312306c5b332ecf14
AbubakarCinnova <amahboob@cinnova.com>
2023-12-08 11:32:22 +0500

    duct cleaning updated

---
+++ duct-cleaning.php
@@ -0,0 +1,84 @@
+<?php include_once('header.php') ?>
+
+<title>Duct Cleaning | Paitson Brothers</title>
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <h2>Residential Air Duct Cleaning</h2>
+
+                                <p><strong>Paitson Bros. Heating and Air Conditioning can help you get cleaner air into
+                                        your home.
+                                        We’re proud to partner with Stanley Steemer to provide you with duct cleaning
+                                        services
+                                        to ensure you are getting the cleanest air possible into your home.</strong></p>
+
+                                <img src="<?php echo $base_url ?>/img/stanley_steamer.png" alt="pic Stanley Steemer"
+                                    style=" width: 100%; padding-top: 20px;" class="page-images">
+                                <br>
+
+                                <h2>Benefits Of Clean Ducts</h2>
+
+                                <ul>
+                                    <li><strong>Better Indoor Air Quality:</strong> The air that circulates through your
+                                        ducts also circulates through your home, so if your ducts are dirty, your indoor
+                                        air quality can suffer. Clean ducts can help ensure that the air you breathe is
+                                        free of contaminants like dust, dirt, pollen, and mold.</li>
+
+                                    <li><strong>Improved Energy Efficiency:</strong> When your ducts are clogged with
+                                        dirt and debris, it can make it harder for your HVAC system to circulate air
+                                        throughout your home. This can lead to increased energy consumption and higher
+                                        utility bills. Clean ducts can help improve your HVAC system's efficiency and
+                                        save you money on energy costs.</li>
+
+                                    <li><strong>Longer HVAC System Life:</strong> A dirty duct system can cause your
+                                        HVAC system to work harder, leading to increased wear and tear on the system. By
+                                        keeping your ducts clean, you can help extend the life of your HVAC system and
+                                        avoid costly repairs or replacements.</li>
+
+                                    <li><strong>Reduced Allergies and Respiratory Issues:</strong> Dirty air ducts can
+                                        contribute to respiratory problems and allergies by circulating contaminants
+                                        throughout your home. Clean ducts can help reduce the number of allergens and
+                                        irritants in your home's air, making it easier for those with respiratory issues
+                                        to breathe.</li>
+
+                                    <li><strong>Improved Comfort:</strong> When your HVAC system is running efficiently,
+                                        it can help keep your home at a more consistent temperature, making it more
+                                        comfortable for you and your family.</li>
+
+                                    <li> <strong>Overall,</strong> clean ducts can improve your home's indoor air
+                                        quality, energy efficiency,
+                                        and comfort, while also helping to extend the life of your HVAC system.</li>
+                                </ul>
+
+                                <br>
+                                <p><strong>We love Stanley Steemer. We know you will too!</strong></p>
+
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Request
+                                    Estimate</a>
+                                <br><br>
+
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
