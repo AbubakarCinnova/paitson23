b11c6feffb4a7fe5536da5cca0eadd4e0b2390b6
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-12-08 17:36:38 +0500

    maintanance plan

---
+++ maintenance-plan.php
@@ -0,0 +1,184 @@
+<?php include_once('header.php') ?>
+
+<title>Annual Planned Service Club | Paitson Brothers</title>
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+                                <p>Paitson Bros. Heating and Air Conditioning is proud to offer residential customers
+                                    three
+                                    levels of membership in our Planned Service Club. These memberships are designed to
+                                    keep
+                                    your system running at its best every month of the year. Each membership includes
+                                    regular seasonal inspections and benefits to ensure your equipment's optimal
+                                    performance
+                                    and to give you the peace of mind you deserve.</p>
+
+                                <p>All memberships include comprehensive seasonal inspections twice per year, a 5%
+                                    discount
+                                    on all new equipment purchases, priority service during peak breakdown events, and
+                                    no
+                                    overtime fees, ever.</p>
+
+                                <h2>Annual Planned Service Club</h2>
+
+                                <p>As your Heating and Cooling equipment ages, the more valuable a maintenance program
+                                    becomes. The savings from one major repair could save you more than the service
+                                    agreement itself. Regular service of your equipment is like changing the oil in your
+                                    vehicle. You can save thousands in energy costs and future repairs, as well as
+                                    extend the life of your HVAC system.</p>
+
+                                <h2>Top Priority Status</h2>
+
+                                <p>You are a Paitson preferred customer when you are a member of the Annual Planned
+                                    Service Club. That means when you need service, you get it NOW - no lengthy waiting
+                                    when your system is out of order. Paitson has emergency service available 24 hours
+                                    per day, every day of the week.</p>
+
+                                <h2>Longer Equipment Life</h2>
+
+                                <p>With the special care, you receive with the Planned Service Club, your heating and
+                                    cooling equipment will last longer and run more efficiently.</p>
+
+                                <h2>Energy Savings</h2>
+
+                                <p>The money you'll save by maintaining your equipment in top running order with a
+                                    Planned Service Club membership will add up to big savings on your heating and
+                                    cooling costs.</p>
+
+                                <h2>Fulfillment of Warranty Requirements</h2>
+
+                                <p>If your HVAC components are under warranty, you should know that most manufacturers
+                                    require evidence of annual maintenance to pay a claim. An investment in regular
+                                    maintenance is essentially giving you an insurance policy. If something malfunctions
+                                    that is covered under warranty, you’ll be able to file your claim with more
+                                    confidence and ease with evidence of annual maintenance.</p>
+
+                                <br>
+
+
+                                <div class="gold">
+                                    <h2>Gold Level Residential Planned Service Club Benefits</h2>
+                                    <ul>
+                                        <li>Free Guaranteed Parts*</li>
+                                        <li>Free labor** On All Repairs</li>
+                                        <li>Multiple System Discounts</li>
+                                        <li>Receive a 5% discount on installed equipment during the contract.</li>
+                                    </ul>
+                                    <p>This all-inclusive package ensures you peace of mind and maximum protection for
+                                        your HVAC system to keep it running smoothly and efficiently all year with the
+                                        premier service you’ve come to expect from Paitson Brothers Heating and Air
+                                        Conditioning.</p>
+                                </div>
+
+                                <div class="silver">
+                                    <h2>Silver Level Residential Planned Service Club Benefits</h2>
+                                    <ul>
+                                        <li>15% Discount On Parts</li>
+                                        <li>Free labor** On All Repairs</li>
+                                        <li>Multiple System Discounts</li>
+                                        <li>Receive a 5% discount on installed equipment during the contract.</li>
+                                    </ul>
+                                    <p>For those residential customers looking for seasonal benefits with additional
+                                        perks, the Silver Plan is the perfect option for you.</p>
+                                </div>
+
+                                <div class="bronze">
+                                    <h2>Bronze Level Residential Planned Service Club Benefits</h2>
+                                    <ul>
+                                        <li>15% Discount On Parts</li>
+                                        <li>15% Discount on Labor for any Necessary Repairs</li>
+                                        <li>Multiple System Discounts</li>
+                                        <li>Receive a 5% discount on installed equipment during the contract.</li>
+                                    </ul>
+                                    <p>Investing in one of our Planned Service Clubs will not only prolong the lifespan
+                                        of your system but also save you money on potential repairs. Join the Paitson
+                                        Bros. family today and ensure your comfort year-round. Contact us to get started
+                                        with the perfect plan for your needs!</p>
+                                </div>
+
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Contact Us</a>
+                                <br>
+
+                                <h2>Your Comprehensive Seasonal Cooling Inspection Includes:</h2>
+                                <ul>
+                                    <li>Check operating pressures and refrigerant charge</li>
+                                    <li>Check voltage and amperage to motors</li>
+                                    <li>Check the temperature drop across the evaporator coil</li>
+                                    <li>Check system superheat and subcooling</li>
+                                    <li>Remove debris from the condenser cabinet</li>
+                                    <li>Check pressure switch</li>
+                                    <li>Check capacitors and relays</li>
+                                    <li>Check contactor</li>
+                                    <li>Check compressor terminals</li>
+                                    <li>Check all wiring and connections</li>
+                                    <li>Check blower condition and operation</li>
+                                    <li>Check and adjust the thermostat</li>
+                                    <li>Clean condenser coil</li>
+                                    <li>Check that the condensate drain is clear</li>
+                                    <li>Check the complete cooling cycle</li>
+                                    <li>Wax outdoor equipment cabinet</li>
+                                    <li>Clean or replace the 1” air filter</li>
+                                    <li>Check disconnect power box</li>
+                                    <li>Add fungicidal tablet to evaporator coil</li>
+                                    <li>Check supply and return static pressure</li>
+                                </ul>
+
+                                <h2>Comprehensive Seasonal Heating Inspection Includes:</h2>
+                                <ul>
+                                    <li>Check safety limits and controls</li>
+                                    <li>Check and adjust the thermostat</li>
+                                    <li>Check pressure switches</li>
+                                    <li>Gas Furnace: check flame sensor / Heat Pump: Check capacitors</li>
+                                    <li>Gas Furnace: check ignition system / Heat Pump: check contactor</li>
+                                    <li>Gas Furnace: check for gas leaks / Heat Pump: check service valves</li>
+                                    <li>Gas Furnace: check incoming gas pressure / Heat Pump: check liquid line
+                                        temperature</li>
+                                    <li>Gas Furnace: check manifold gas pressure / Heat Pump: check suction line
+                                        temperature</li>
+                                    <li>Gas Furnace: check exhaust and drain / Heat Pump: check defrost cycle</li>
+                                    <li>Gas Furnace: check the heat exchanger / Heat Pump: check the indoor coil</li>
+                                    <li>Check voltage and amperage to motors</li>
+                                    <li>Vacuum indoor cabinet</li>
+                                    <li>Check blower condition and operation</li>
+                                    <li>Check temperature rise</li>
+                                    <li>Check the complete heating cycle</li>
+                                    <li>Wax indoor equipment cabinet</li>
+                                    <li>Clean or replace the 1” air filter</li>
+                                    <li>Check the power breaker and SSU</li>
+                                    <li>Check carbon monoxide and smoke detectors</li>
+                                    <li>Check supply and return static pressure</li>
+                                </ul>
+
+                                <p>The per-year price is for a standard single-family home</p>
+
+                                <p>* Compressor or heat exchanger repairs are covered only if the compressor or heat
+                                    exchanger is covered by the manufacturer’s warranty. <br>
+                                    **Labor covered for compressor or heat exchanger repairs is covered only if the
+                                    compressor or heat exchanger is covered by the manufacturer’s warranty.
+                                </p>
+
+                                <br>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
