7803595b7fca107b6f9cdb3803f3436323350482
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-12-13 15:54:33 +0500

    Create generators.php

---
+++ generators.php
@@ -0,0 +1,158 @@
+<?php include_once('header.php') ?>
+
+<title>Automatic Whole Home Generators | Paitson Brothers</title>
+<meta name="description"
+    content=" In Terre Haute, where severe weather and prolonged power outages are not uncommon, trust Paitson Bros., your local Generac PowerPro dealer, for Automatic Whole Home Generators. ">
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+                                <h1>Automatic Whole Home Generators</h1>
+
+                                <p>Experience the unmatched convenience and assurance of Automatic Whole Home Generators
+                                    with Paitson Bros. As a trusted Generac PowerPro dealer, we deeply understand the
+                                    significance of reliable energy in safeguarding your valuable investments, providing
+                                    you
+                                    with peace of mind during unpredictable weather events. Discover the simplicity and
+                                    affordability of a new generator installation for your home or business.</p>
+                                <p>In Terre Haute, where severe weather and prolonged power outages are not uncommon,
+                                    trust
+                                    Paitson Bros., your local Generac PowerPro dealer, for Automatic Whole Home
+                                    Generators.
+                                    Our generators keep your home fully powered during storms and outages, providing
+                                    both
+                                    comfort and security. Discover the convenience and reliability of Automatic Whole
+                                    Home
+                                    Generators with Paitson Bros., ensuring uninterrupted power for your home in
+                                    challenging
+                                    times.</p>
+
+                                <h2>Here are some of the advantages of having an Automatic Whole Home Generator:</h2>
+
+                                <ul>
+                                    <li>
+                                        <strong>Exceptional Convenience:</strong> Whole-house generators are seamlessly
+                                        integrated into your home's electrical system, enabling them to detect
+                                        interruptions in electricity and automatically activate to supply the power you
+                                        require. No manual initiation is necessary – these systems conveniently start on
+                                        their own!
+                                    </li>
+                                    <li>
+                                        <strong>Stress Relief:</strong> Whole-house generators alleviate anxiety for
+                                        those grappling with power outages caused by storms and dealing with uncertain
+                                        restoration timelines promised by power companies. Possessing your own Automatic
+                                        Whole Home Generator system provides remarkable peace of mind, especially in
+                                        moments when maintaining the normal functioning of your home might be a cause
+                                        for concern.
+                                    </li>
+                                    <li>
+                                        <strong>Furnace Power:</strong> Ensure warmth during winter outages by powering
+                                        your furnace, as all furnaces need electrical power to operate. Your Automatic
+                                        Whole Home Generator keeps the heat on and you comfortable, even in the coldest
+                                        conditions.
+                                    </li>
+                                    <li>
+                                        <strong>Appliance Continuity:</strong> Whole-house generators ensure
+                                        uninterrupted power for essential appliances, protecting your food supply and
+                                        preventing inconvenience. From refrigerators and freezers to lifesaving medical
+                                        equipment and entertainment devices like TVs, these generators also ensure
+                                        essential lighting during outages.
+                                    </li>
+                                    <li>
+                                        <strong>Summer Comfort:</strong> Enjoy uninterrupted air conditioning and stay
+                                        cool even during power outages. Whole-house generators deliver full-home power,
+                                        ensuring a seamless continuation of your daily life when the grid goes dark.
+                                        Don't endure discomfort or sit in the dark like your neighbors during a summer
+                                        power outage.
+                                    </li>
+                                    <li>
+                                        <strong>Protection Against Power Fluctuations:</strong> Shield your appliances
+                                        and electronics from power fluctuations and surges. A steady power supply from
+                                        the Automatic Whole Home Generator prevents damage until the power grid is
+                                        restored to reliability.
+                                    </li>
+                                    <li>
+                                        <strong>Rural Water Supply:</strong> Ensure a constant supply of clean water for
+                                        homes with electric wells. Automatic Whole Home Generators keep electric wells
+                                        operational, preventing interruptions in water access during power outages.
+                                    </li>
+                                    <li>
+                                        <strong>Communication Support:</strong> Charge your phone to stay connected
+                                        during emergencies. In a power outage, communication is vital, and a charged
+                                        phone allows you to inform loved ones of your well-being and receive updates
+                                        from emergency services.
+                                    </li>
+                                    <li>
+                                        <strong>Security Enhancement:</strong> Deter potential burglars by making your
+                                        home less attractive during power outages. A running generator creates the
+                                        illusion of an occupied and protected home, potentially reducing your home
+                                        insurance rates.
+                                    </li>
+                                    <li>
+                                        <strong>Comfort During Storms:</strong> Enhance your comfort during storms.
+                                        Whether it's avoiding obstacles in the dark, maintaining a comfortable
+                                        temperature, or simply having a more enjoyable experience, an Automatic Whole
+                                        Home Generator ensures a more comfortable environment during power outages.
+                                    </li>
+                                </ul>
+
+
+                                <p><strong>Choose the reliability and peace of mind that Automatic Whole Home Generators
+                                        from
+                                        Paitson Bros. provide. Don't let power outages disrupt your life – invest in the
+                                        comfort
+                                        and security of a generator today.</strong></p>
+
+
+                                <p>Paitson Bros. offers a Generac generator package you can't find anywhere else: all
+                                    necessary installation equipment, a turn-key installation package that includes all
+                                    necessary site prep, gas piping, electrical work, and a comprehensive warranty that
+                                    goes
+                                    above and beyond what Generac offers alone. And since we're the Wabash Valley's only
+                                    authorized service dealer, you know you'll always be taken care of.</p>
+
+
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/" class="btn btn-warning"> Explore our
+                                    Generator models</a>
+                                <br>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                <br>
+
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Request
+                                    Quote</a>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
