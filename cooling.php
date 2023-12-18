737d4a977174d4709803362df13c6126d467765b
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-12-18 15:34:31 +0500

    updated cooling page

---
+++ cooling.php
@@ -0,0 +1,137 @@
+<?php include_once('header.php') ?>
+<title>Paitson Bros. Heating and Air Conditioning| Cooling</title>
+
+
+
+<meta name="description"
+    content=" At Paitson Bros. Heating and Air Conditioning, our friendly and skilled technicians are ready to diagnose and swiftly resolve any issues with your air conditioning system." />
+
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <img src="<?php echo $base_url ?>/img/cooling-banner.png" alt="cooling-banner"
+                                    class="full-width-img">
+                                <br>
+
+                                <h2>Air Conditioner Repair Services</h2>
+
+                                <p>Struggling with an air conditioning system that isn't cooling your space effectively?
+                                    Whether your central AC unit needs a recharge or is completely non-functional, worry
+                                    not! At Paitson Bros. Heating and Air Conditioning, our friendly and skilled
+                                    technicians
+                                    are ready to diagnose and swiftly resolve any issues with your air conditioning
+                                    system.
+                                    Specializing in the repair of all brands and types of heating and cooling equipment,
+                                    we
+                                    ensure prompt restoration of your comfort.</p>
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Request
+                                    Service</a>
+                                <br>
+
+
+                                <h2>Air Conditioner Maintenance</h2>
+
+                                <p>Don't suffer through the scorching Indiana summer in a hot home. Rely on Paitson
+                                    Bros.
+                                    Heating and Air Conditioning for expert air conditioning maintenance that prevents
+                                    breakdowns in your cooling system. Regular AC maintenance can prolong the lifespan
+                                    of
+                                    your central air system and help manage your expenses. Explore our maintenance plans
+                                    at
+                                    Paitson Bros. Heating and Air Conditioning, ensuring a trouble-free summer for your
+                                    HVAC
+                                    system.</p>
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/maintenance-plan.php" class="btn btn-warning">View our
+                                    Maintenance Plans</a>
+                                <br>
+
+                                <h2>High-Efficiency Air Conditioning Systems that Save You Money</h2>
+                                <p>Selecting the right air conditioning system is a crucial decision for homeowners,
+                                    with factors like efficiency, comfort, and budget playing key roles. Discover which
+                                    Lennox model best suits your home by contacting us today for a free estimate and
+                                    consultation. A Paitson Bros. comfort designer will arrange a visit to your home,
+                                    explaining the available heating and cooling systems tailored for your space.
+                                    They'll present all options, helping you strike the ideal balance between comfort,
+                                    efficiency, and cost.</p>
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/air-conditioners.php" class="btn btn-warning">View Air
+                                    Conditioners</a>
+                                <br>
+
+
+                                <h2>Ductless Mini Split</h2>
+                                <p>A ductless mini-split system offers an efficient heating and cooling solution by
+                                    delivering conditioned air directly to individual rooms without the need for ducts.
+                                    Also known as mini-splits, multi-splits, heat pumps, or ductless air conditioning
+                                    systems, these units are ideal for homes without traditional ductwork, as well as
+                                    for garages, workshops, and additions. They provide an excellent option if you want
+                                    to add AC to a specific room or area without the need for modifications to existing
+                                    ductwork.</p>
+
+                                <h2>We Offer Sales, Service, and Installation of:</h2>
+                                <ul>
+                                    <li>Air Conditioning Installation</li>
+                                    <li>Residential and Commercial</li>
+                                    <li>New Home Construction Comfort Systems</li>
+                                    <li>HVAC Replacement</li>
+                                    <li>AC recharging</li>
+                                    <li>Air Conditioning Repairs</li>
+                                    <li>High-Efficiency Air Conditioning</li>
+                                    <li>Air Conditioning Repair</li>
+                                    <li>Maintenance Plans</li>
+                                    <li>Ductless Mini Split Systems</li>
+                                    <li>Central Air Repair</li>
+                                    <li>AC Repair</li>
+                                    <li>Duct Cleaning</li>
+                                    <li>Ductwork Repairs and Replacements</li>
+                                    <li>UV Germicidal Lights</li>
+                                    <li>WiFi Thermostats</li>
+                                    <li>Affordable Pricing</li>
+                                    <li>Lennox Dealer</li>
+                                    <li>RUUD Dealer</li>
+                                    <li>Indoor Air Quality Installation</li>
+                                    <li>Emergency Service</li>
+                                    <li>Whole House Humidifiers</li>
+                                    <li>Whole House Air Purifiers</li>
+                                </ul>
+                                <br>
+
+                                <p><strong>Paitson Bros. Heating and Air Conditioning: A Century of Comfort, A Legacy of
+                                        Excellence!</strong></p>
+
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Request
+                                    Service</a>
+                                <br>
+
+
+
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
