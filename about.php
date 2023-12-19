782395ff57a1906d4a025d565bac06a17efe2104
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-12-19 16:50:54 +0500

    Create about.php

---
+++ about.php
@@ -0,0 +1,80 @@
+<?php include_once('header.php') ?>
+<title>Paitson Bros. Heating and Air Conditioning | About </title>
+
+
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+
+
+                                <h1>About</h1>
+
+                                <!-- <img src="<?php echo $base_url ?>/img/about"
+                                    alt="about banner" class="full-width-img"> -->
+
+                                <h2>Introduction:</h2>
+                                <p>With a rich history dating back to 1922, Paitson Bros. Heating and Air Conditioning
+                                    has been a cornerstone in serving the Wabash Valley for over a century. This is the
+                                    story of our commitment to excellence, led by Terre Haute native and Purdue
+                                    University alumnus, Ethan Rayburn.</p>
+
+                                <h2>Our Legacy:</h2>
+                                <p>Since its establishment in 1922, Paitson Bros. has been a trusted provider of
+                                    heating, air conditioning, and power generation services. Our legacy is rooted in a
+                                    dedication to superior service, prompt response, and competitive pricing.</p>
+
+                                <h2>Ethan Rayburn's Leadership:</h2>
+                                <p>At the helm is Ethan Rayburn, a proud Terre Haute native and graduate of Terre Haute
+                                    North Vigo High School. Ethan's journey led him to Purdue University, where he honed
+                                    his skills. Since joining Paitson Bros. in 2010, he has passionately led our team to
+                                    deliver unparalleled service.</p>
+
+                                <h2>Community Engagement:</h2>
+                                <p>Beyond business, we are an integral part of the community. Ethan actively
+                                    participates in various organizations, including serving as the president of the
+                                    Vigo County Youth Soccer Association and contributing to the Real Men Read program.
+                                    Our commitment extends to charitable initiatives, such as Operation Warm Christmas,
+                                    which provides free heating systems to deserving families.</p>
+
+                                <h2>Your Comfort, Our Priority:</h2>
+                                <p>When you choose Paitson Bros. Heating and Air Conditioning, you're not just selecting
+                                    a service provider; you're joining a community. Our experienced team ensures your
+                                    comfort and our commitment to giving back reflects our dedication to the Wabash
+                                    Valley.</p>
+
+                                <h2>Contact Us:</h2>
+                                <p>Thank you for considering Paitson Bros. Heating and Air Conditioning. For exceptional
+                                    service and a community-focused approach, contact us today. We look forward to
+                                    serving you and your family with integrity, expertise, and a genuine dedication to
+                                    your comfort and satisfaction.</p>
+
+
+
+                                <div>
+                                    <iframe src="https://player.vimeo.com/video/895836663" width="640" height="360"
+                                        class="video-responsive" frameborder="0" allow="autoplay; fullscreen"
+                                        allowfullscreen></iframe>
+
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
