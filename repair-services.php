32004bc9d978efaba9dd3458c4eb92aa0d8b28e0
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-12-19 12:47:48 +0500

    Create repair-services.php

---
+++ repair-services.php
@@ -0,0 +1,108 @@
+<?php include_once('header.php') ?>
+<title>Repair |Paitson Brothers</title>
+<meta name="description"
+    content=" At Paitson Bros. Heating and Air Conditioning, our trained technicians will expertly diagnose, repair, and service your furnace, AC or HVAC to restore maximum efficiency and comfort." />
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+
+                                <!-- <img src="<?php echo $base_url ?>/img/lennox furnace banner.jpg"
+                                    alt="furnace banner" class="full-width-img"> -->
+
+                                <h2>Repair Services</h2>
+                                <p>We provide 24-hour emergency services and work on brands of residential and
+                                    commercial heating and cooling equipment.</p>
+                                <p>For Emergency Service, Toll-Free: <strong>(800) 533-0711</strong> Right Now!</p>
+
+                                <h2>FURNACE REPAIR SERVICES</h2>
+                                <p>At Paitson Bros. Heating and Air Conditioning, our trained technicians will expertly
+                                    diagnose, repair, and service your furnace or HVAC to restore maximum efficiency and
+                                    comfort. We repair furnaces including electric furnaces, gas furnaces, and heat
+                                    pumps for residential and commercial.</p>
+
+                                <h2>AIR CONDITIONER REPAIR SERVICES</h2>
+                                <p>Is your air conditioning system not providing cooling to your home or business well?
+                                    Does your AC central air unit need to be recharged or does it not run at all?
+                                    Whatever the issue, Paitson Bros. Heating and Air Conditioning's friendly and expert
+                                    technicians can quickly diagnose and repair your air conditioning system problems.
+                                </p>
+
+                                <h2>Annual Planned Service Club</h2>
+                                <p> As your Heating and Cooling equipment ages, the more valuable a maintenance program
+                                    becomes. The savings from one major repair could save you more than the service
+                                    agreement itself. Regular service of your equipment is like changing the oil in your
+                                    vehicle. You can save thousands in energy costs and future repairs, as well as
+                                    extend the life of your HVAC system. <a
+                                        href="<?php echo $base_url ?>/maintenance-plan.php">Learn
+                                        more about our Planned Service Club</a></p>
+
+                                <h2>We Offer Sales, Service, and Installation of:</h2>
+                                <ul>
+                                    <li>Air Conditioning Systems</li>
+                                    <li>High-Efficiency Furnaces</li>
+                                    <li>Furnace Repairs</li>
+                                    <li>Heat Pumps</li>
+                                    <li>Air Conditioning Repairs</li>
+                                    <li>Central Air Systems</li>
+                                    <li>24-hour emergency services</li>
+                                    <li>Commercial HVAC</li>
+                                    <li>HVAC</li>
+                                    <li>AC Recharging</li>
+                                    <li>High-Efficiency Air Conditioning</li>
+                                    <li>Ductless Mini Split Systems</li>
+                                    <li>Ductwork Repairs and Replacements</li>
+                                    <li>UV Germicidal Lights</li>
+                                    <li>Thermostats</li>
+                                    <li>Whole House Humidifiers</li>
+                                    <li>Smart Thermostats</li>
+                                    <li>Indoor Air Quality Systems</li>
+                                    <li>24/7 Emergency Services</li>
+                                    <li>Automatic Whole Home Generators</li>
+                                </ul>
+
+                                <h2>OUR SERVICES ARE THE BEST BECAUSE WE:</h2>
+                                <ul>
+                                    <li>Provide top-notch customer service</li>
+                                    <li>Have been in the HVAC repair business for over 100 years</li>
+                                    <li>Offer free estimates on new HVAC system installations</li>
+                                    <li>Provide flexible financing options with approved credit</li>
+                                    <li>Are a local licensed Lennox and Generac dealer</li>
+                                    <li>Offer promotions and rebates throughout the year</li>
+                                    <li>Provide competitive pricing</li>
+                                    <li>Are family-owned and operated</li>
+                                    <li>Receive consistent 5-star reviews from our customers</li>
+                                </ul>
+                                <br>
+
+                                <p><strong>Paitson Bros. Heating and Air Conditioning: A Century of Comfort, A Legacy of
+                                        Excellence!</strong></p>
+
+
+
+
+                                <br>
+
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Request
+                                    Service</a>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
