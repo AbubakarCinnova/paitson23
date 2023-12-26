125a19cf7e0964cd10eef87746ff37b904ad793c
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-12-26 11:00:53 +0500

    Create heating.php

---
+++ heating.php
@@ -0,0 +1,143 @@
+<?php include_once('header.php') ?>
+
+<title>Heating | Paitson Brothers</title>
+<meta name="description"
+    content=" Swift furnace solutions by Paitson Bros. 24/7 emergency services, furnace repairs, replacements, and high-efficiency installations. Trust a century of HVAC expertise. Contact us today!">
+
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-md-12">
+
+                                <h1>Expert Furnace Solutions by Paitson Bros.</h1>
+
+                                <p>If your furnace is causing issues, Paitson Bros. Heating and Air Conditioning are
+                                    ready to provide swift and effective solutions. Our skilled technicians handle
+                                    repairs for all types of furnaces, including gas, electric, dual fuel, and heat pump
+                                    systems. With our 24/7 emergency services, we guarantee swift resolution of your
+                                    furnace concerns, ensuring you can quickly resume your routine without unnecessary
+                                    disruptions.</p>
+
+                                <h2>Furnace Lifespan and Upgrades:</h2>
+
+                                <h3>Understanding Furnace Lifespan:</h3>
+                                <p>Furnaces typically last 15 to 20 years with proper maintenance. Regular maintenance
+                                    is key to extending lifespan and maintaining efficiency.</p>
+
+                                <h3>Furnace Efficiency, Innovation, and Safety:</h3>
+                                <ul>
+                                    <li><strong>Efficiency:</strong> Newer furnaces boast higher energy efficiency,
+                                        leading to lower utility bills and reduced environmental impact.</li>
+                                    <li><strong>Innovation:</strong> Technological advancements introduce features like
+                                        smart thermostats and zoning controls, enhancing comfort and control.</li>
+                                    <li><strong>Safety:</strong> Older furnaces may pose safety risks; upgrading ensures
+                                        compliance with current safety standards.</li>
+                                </ul>
+
+                                <p>When it comes to furnace replacement and installation, Paitson Bros. offers a
+                                    cost-effective solution. We specialize in installing high-efficiency Lennox furnaces
+                                    and customizing systems to fit your family's comfort needs and budget. Our
+                                    commitment to energy efficiency extends to comprehensive furnace maintenance checks.
+                                    <a href="<?php echo $base_url ?>/furnaces.php">Learn
+                                        more</a>
+                                </p>
+
+                                <h2>Paitson Bros. Heating Services:</h2>
+
+                                <h3>High-Efficiency Solutions:</h3>
+                                <ul>
+                                    <li>High-Efficiency Furnaces</li>
+                                    <li>High-Efficiency Heat Pumps</li>
+                                </ul>
+
+                                <h3>Repairs and Replacements:</h3>
+                                <ul>
+                                    <li>Gas Furnace Repair</li>
+                                    <li>Furnace Replacement</li>
+                                    <li>Boiler Services</li>
+                                    <li>Ductwork Repairs and Replacement</li>
+                                    <li>Central Air Repair</li>
+                                    <li>Ductless Mini Split</li>
+                                </ul>
+
+                                <h3>Indoor Air Quality:</h3>
+                                <ul>
+                                    <li>Air Purification Systems</li>
+                                    <li>Whole House Humidifiers</li>
+                                    <li>Duct Cleaning</li>
+                                </ul>
+
+                                <h3>Smart Solutions:</h3>
+                                <ul>
+                                    <li>Programmable Thermostats</li>
+                                    <li>Smart WiFi Thermostats</li>
+                                </ul>
+
+                                <h3>Emergency Services:</h3>
+                                <ul>
+                                    <li>24/7 Emergency Furnace Services</li>
+                                </ul>
+
+                                <h3>Additional Services:</h3>
+                                <ul>
+                                    <li>Dual Fuel Systems</li>
+                                    <li>Heating System Upgrades</li>
+                                    <li>Ductwork</li>
+                                </ul>
+
+                                <h2>Why Choose Paitson Bros. Heating and Air Conditioning?</h2>
+
+                                <ul>
+                                    <li><strong>Top-notch Customer Service:</strong> Our services are backed by
+                                        excellent customer support.</li>
+                                    <li><strong>Over 100 Years in HVAC Business:</strong> Trust our legacy and
+                                        expertise.</li>
+                                    <li><strong>Free Estimates:</strong> Receive free estimates on new HVAC system
+                                        installations.</li>
+                                    <li><strong>Flexible Financing:</strong> We offer financing options with approved
+                                        credit.</li>
+                                    <li><strong>Local Lennox and Generac Dealer:</strong> Authorized dealer for quality
+                                        products.</li>
+                                    <li><strong>Promotions and Rebates:</strong> Take advantage of promotions and
+                                        rebates.</li>
+                                    <li><strong>Competitive Pricing:</strong> Our pricing is competitive in the market.
+                                    </li>
+                                    <li><strong>Family-Owned and Operated:</strong> We prioritize a family-oriented
+                                        approach.</li>
+                                    <li><strong>Consistent 5-Star Reviews:</strong> Read what our satisfied customers
+                                        say about us.</li>
+                                </ul>
+
+                                <p><strong>Paitson Bros. Heating and Air Conditioning:</strong> A Century of Comfort, A
+                                    Legacy of Excellence!</p>
+
+                                <p>For prompt and reliable furnace services, <a
+                                        href="<?php echo $base_url ?>/contact-us.php" target="_self">contact us
+                                        today</a>!</p>
+
+
+                                <br>
+
+
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
