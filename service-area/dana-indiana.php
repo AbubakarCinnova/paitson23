a9694f9be88962bc77661152e48d69c689f1cbf4
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-01-04 12:56:42 +0500

    dana updated

---
+++ service-area/dana-indiana.php
@@ -0,0 +1,164 @@
+<?php include_once('../header.php') ?>
+
+<title>Furnace Repair Clay City, IN | AC Repair| HVAC Clay City, IN| Paitson Bros. </title>
+
+
+
+
+<meta name="description"
+    content=" Clay City Indiana's Premier HVAC and Furnace Repair Experts: Paitson Bros. Heating and Air Conditioning.  A Century of Comfort, A Legacy of Excellence " />
+
+
+
+<meta name="rating" content="general" />
+<meta name="revisit-after" content="3 days" />
+<meta name="robots" content="index, follow" />
+<meta name="author" content="CG" />
+
+<?php include_once('../menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <h1>Dana, Indiana</h1>
+
+
+                                <img src="<?php echo $base_url ?>/img/Dana, Indiana.jpg" alt="pic Dana, Indiana"
+                                    style=" width: 100%; padding: 20px;" class="duct-clean-img">
+                                <br>
+
+                                <p>Nestled among expansive farms, Dana, Indiana, is a charming small town with a rich
+                                    history. Renowned as the birthplace of the famous journalist Ernie Pyle, Dana
+                                    proudly hosts a museum dedicated to showcasing his life and writings. A town steeped
+                                    in heritage and surrounded by picturesque landscapes, Dana invites visitors to
+                                    explore its quaint charm and the impactful legacy of one of America's notable war
+                                    correspondents from World War II.
+                                </p>
+
+                                <h1>Dana, Indiana's Premier HVAC and Furnace Repair Experts: Paitson Bros. Heating and
+                                    Air Conditioning - A Century of Comfort, A Legacy of Excellence</h1>
+
+                                <p>Discover superior HVAC services right here in Dana, Indiana with Paitson Bros.
+                                    Heating and Air Conditioning. For over 100 years, we've been the trusted choice for
+                                    homeowners in Dana, Indiana, and its neighboring regions. Whether you need furnace
+                                    repairs, installations, or top-tier air conditioning services, our dedicated team is
+                                    committed to ensuring your home stays comfortable. Count on us for 24/7 emergency
+                                    services and comprehensive HVAC solutions tailored to your needs.</p>
+
+                                <h2>Dana, Indiana Air Conditioning Services</h2>
+
+                                <p>Stay cool in Dana's summer heat with Paitson Bros. Heating and Air Conditioning! Our
+                                    comprehensive air conditioning services cater to all your needs, from repair and
+                                    replacement to recharging and maintenance for both residential and commercial
+                                    properties in Dana, Indiana. As your dependable 24-hour emergency HVAC service
+                                    provider, we take pride in our proficiency with all major brands of home cooling
+                                    equipment, ensuring you receive exceptional service and performance every time.
+                                    Additionally, explore our maintenance plans that deliver great benefits at an
+                                    affordable price. <a href="<?php echo $base_url ?>/maintenance-plan.php">Learn
+                                        more</a></p>
+
+                                <h3>Air Conditioning Services Dana, IN</h3>
+
+                                <ul>
+                                    <li>Energy-Efficient Central Air Systems</li>
+                                    <li>Cooling Repair</li>
+                                    <li>Commercial HVAC</li>
+                                    <li>AC Replacements</li>
+                                    <li>Air Conditioning Repair</li>
+                                    <li>AC Repair</li>
+                                    <li>New Home HVAC Installation</li>
+                                    <li>New AC Installation</li>
+                                    <li>High Efficiency HVAC</li>
+                                    <li>Central Air Repair</li>
+                                    <li>AC Tune-Ups</li>
+                                    <li>High-Efficiency AC</li>
+                                    <li>24/7 Emergency Services</li>
+                                    <li>Mini Splits</li>
+                                    <li>Smart Thermostats</li>
+                                </ul>
+
+                                <h2>Furnace Repair Services in Dana, IN</h2>
+
+                                <p>Count on Paitson Bros. Heating and Air Conditioning for comprehensive furnace repair
+                                    services in Dana, Indiana. Whether you need a new furnace installation, replacement,
+                                    or swift repairs for your Dana residence, our dedicated team is committed to
+                                    ensuring your home stays warm and cozy. We take pride in offering 24/7 emergency
+                                    furnace repair services and possess expertise in servicing all brands of home
+                                    heating equipment. Rest assured, your furnace is in capable hands with us.</p>
+
+                                <h3>Furnace Services Dana, IN</h3>
+
+                                <ul>
+                                    <li>Furnace Repair</li>
+                                    <li>Heating Repair</li>
+                                    <li>High Efficiency Furnaces</li>
+                                    <li>24/7 Emergency Furnace Repair</li>
+                                    <li>New Furnace Installation</li>
+                                    <li>Heat Pumps</li>
+                                    <li>Dual Fuel Systems</li>
+                                    <li>Energy Efficient Furnaces</li>
+                                    <li>High-Efficiency Heat Pumps</li>
+                                    <li>Heating and Furnace Maintenance</li>
+                                    <li>New Gas Furnace Installation</li>
+                                    <li>HVAC Repairs</li>
+                                    <li>Duct Cleaning</li>
+                                    <li>Whole Home Humidifiers</li>
+                                    <li>Central Air Repair</li>
+                                    <li>WiFi Thermostats</li>
+                                    <li>Lennox Dealer</li>
+                                    <li>NATE Certification Techs</li>
+                                    <li>HVAC Replacement</li>
+                                    <li>Smart Thermostats</li>
+                                </ul>
+
+                                <h2>Dana, Indiana HVAC Installations</h2>
+
+                                <p>Upgrade your home's comfort system with a new Lennox high-efficiency heating and
+                                    cooling system from Paitson Bros. Heating and Air Conditioning in Dana. Enjoy
+                                    substantial energy savings and year-round comfort. Our Dana HVAC solutions span
+                                    various budgets, and we specialize in advanced technology and smart thermostat
+                                    control for effortless system management. Trust our expert Dana-based team for
+                                    top-notch HVAC installations.</p>
+
+                                <h2>Whole Home Generators, Dana, IN</h2>
+
+                                <p>Discover unmatched convenience with Whole Home Generators installed in Dana by
+                                    Paitson Bros. As your trusted Generac PowerPro dealer, we offer decades of
+                                    experience ensuring reliable energy solutions for your home or business, especially
+                                    during unpredictable weather events. Explore seamless installation and
+                                    cost-effective options. Trust Paitson Bros. for efficient power solutions in Dana,
+                                    IN backed by our expertise as a Generac PowerPro dealer. <a
+                                        href="<?php echo $base_url ?>/generators.php">Learn
+                                        More</a></p>
+
+                                <p>Paitson Bros. Heating and Air Conditioning: A Century of Comfort, A Legacy of
+                                    Excellence in Dana Indiana</p>
+
+
+
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Contact
+                                    Us</a>
+                                <br><br>
+
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('../footer.php'); ?>
