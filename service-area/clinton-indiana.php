f3b62ef88a6c282f8fd2ba26d526a787e12e5ca5
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-01-04 12:26:56 +0500

    clinton updated

---
+++ service-area/clinton-indiana.php
@@ -0,0 +1,60 @@
+<?php include_once('../header.php') ?>
+
+<title>Furnace Repair Clinton, IN | AC Repair| HVAC Clinton, IN| Paitson Bros. </title>
+<meta name="description"
+    content=" Clinton Indiana's Premier HVAC and Furnace Repair Experts: Paitson Bros. Heating and Air Conditioning.  A Century of Comfort, A Legacy of Excellence " />
+<meta name="rating" content="general" />
+<meta name="revisit-after" content="3 days" />
+<meta name="robots" content="index, follow" />
+<meta name="author" content="CG" />
+
+<?php include_once('../menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <h1>Clinton, Indiana</h1>
+
+
+                                <img src="<?php echo $base_url ?>/img/clinton.jpg" alt="pic clinton"
+                                    style=" width: 100%; padding-top: 20px;" class="duct-clean-img">
+                                <br>
+
+                                <p>Amid the beautiful and fertile farmlands of Western Indiana is the charming city of
+                                    Clinton. Clinton, Indiana is located in Vermillion County and was founded by Italian
+                                    immigrants in 1829 as a coal mining center. Every September, Clinton hosts the
+                                    annual Little Italy Festival, a four-day Labor Day Weekend celebration of the area’s
+                                    Italian and coal mining heritage. Founded in 1966, this favorite Clinton, Indiana
+                                    event draws over 75,000 visitors and features parades, wonderful Italian food, grape
+                                    stomping, live music, and some great down-home hospitality. The city of Clinton is
+                                    alive in the present while preserving its past.
+                                </p>
+
+
+
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Request
+                                    Estimate</a>
+                                <br><br>
+
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('../footer.php'); ?>
