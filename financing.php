9ed4b4a734e6e655e95f050e55df0b7fa5e292a4
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-01-05 12:07:37 +0500

    financing page updates

---
+++ financing.php
@@ -0,0 +1,48 @@
+<?php include_once('header.php') ?>
+
+<title>Paitson Bros. Heating and Air Conditioning | Financing</title>
+
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+
+                                <h1>Financing</h1>
+
+                                <p>CONVENIENT FINANCING OPTIONS MAKE YOUR PURCHASE EASY AND COMFORTABLE.
+                                </p>
+
+                                <p>Financing a new home heating and cooling system is one of the most important
+                                    investments you'll make, one that can deliver energy and cost savings for many years
+                                    to come. You don't have to let a small budget keep you from getting the most
+                                    efficient solution for your household.
+                                </p>
+                                <br>
+                                <a href="https://apply.svcfin.com/home/dealerAuthentication?id=400106721&key=1742219857"
+                                    target="_blank"><img src="<?php echo $base_url ?>/img/Service-Finance.png"
+                                        alt="service finance image" width="300"></a>
+
+                                <br>
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
