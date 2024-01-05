13c19941d6e2654cbc49ea0b6da727f69e8eb31a
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-01-05 11:34:49 +0500

    paris page updated

---
+++ service-area/paris-illinois.php
@@ -0,0 +1,153 @@
+<?php include_once('../header.php') ?>
+<title>Furnace Repair Paris, IL| AC Repair| HVAC Paris, IL| Paitson Bros. </title>
+
+<meta name="description"
+    content="Paris Illinois's Premier HVAC and Furnace Repair Experts: Paitson Bros. Heating and Air Conditioning.  A Century of Comfort, A Legacy of Excellence " />
+
+<meta name="rating" content="general" />
+<meta name="revisit-after" content="3 days" />
+<meta name="robots" content="index, follow" />
+<meta name="author" content="CG" />
+
+
+<?php include_once('../menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <h1>Paris, Illinois</h1>
+
+
+                                <img src="<?php echo $base_url ?>/img/paris illinois.jpg" alt="Paris Illinois"
+                                    style=" width: 100%; padding: 20px;" class="duct-clean-img">
+                                <br>
+
+                                <h2>Paris Illinois's Premier HVAC and Furnace Repair Experts: Paitson Bros. Heating and
+                                    Air Conditioning - A Century of Comfort, A Legacy of Excellence
+                                </h2>
+                                <p>Explore exceptional HVAC services in Paris, Illinois, courtesy of Paitson Bros.
+                                    Heating and Air Conditioning. With a century of experience, we've been the preferred
+                                    option for homeowners in Paris and the surrounding areas. Whether you require
+                                    furnace repairs, installations, or premium air conditioning services, our devoted
+                                    team is dedicated to maintaining the comfort of your home. Rely on us for
+                                    round-the-clock emergency services and customized HVAC solutions to meet your
+                                    specific needs.
+
+                                </p>
+
+                                <h2>Paris, Illinois Air Conditioning Services</h2>
+
+                                <p>Stay cool in the Paris Illinois summer heat with Paitson Bros. Heating and Air
+                                    Conditioning! Our comprehensive air conditioning services cover everything you need,
+                                    from repair and replacement to recharging and maintenance for both residential and
+                                    commercial properties in Paris. We're your reliable 24-hour emergency HVAC service
+                                    provider. We pride ourselves on working with all major brands of home cooling
+                                    equipment, ensuring you receive exceptional service and performance every time. We
+                                    also offer maintenance plans that offer great benefits at a great price. <a
+                                        href="<?php echo $base_url ?>/maintenance-plan.php">Learn
+                                        more</a></p>
+
+                                <h3>Air Conditioning Services Paris, IL</h3>
+
+                                <ul>
+                                    <li>Energy-Efficient Central Air Systems</li>
+                                    <li>Cooling Repair</li>
+                                    <li>Commercial HVAC</li>
+                                    <li>AC Replacements</li>
+                                    <li>Air Conditioning Repair</li>
+                                    <li>AC Repair</li>
+                                    <li>New Home HVAC Installation</li>
+                                    <li>New AC Installation</li>
+                                    <li>High Efficiency HVAC</li>
+                                    <li>Central Air Repair</li>
+                                    <li>AC Tune-Ups</li>
+                                    <li>High-Efficiency AC</li>
+                                    <li>24/7 Emergency Services</li>
+                                    <li>Mini Splits</li>
+                                    <li>Smart Thermostats</li>
+                                </ul>
+
+                                <h2>Furnace Repair Services in Paris, Illinois</h2>
+
+                                <p>Count on Paitson Bros. Heating and Air Conditioning for comprehensive furnace repair
+                                    services in Paris. Whether you need a new furnace installation, replacement, or
+                                    swift repairs for your Paris residence, our dedicated team is committed to ensuring
+                                    your home stays warm and cozy. We take pride in offering 24/7 emergency furnace
+                                    repair services and possess expertise in servicing all brands of home heating
+                                    equipment. Rest assured, your furnace is in capable hands with us.</p>
+
+                                <h3>Furnace Services Paris, Illinois</h3>
+
+                                <ul>
+                                    <li>Furnace Repair</li>
+                                    <li>Heating Repair</li>
+                                    <li>High Efficiency Furnaces</li>
+                                    <li>24/7 Emergency Furnace Repair</li>
+                                    <li>New Furnace Installation</li>
+                                    <li>Heat Pumps</li>
+                                    <li>Duel Fuel Systems</li>
+                                    <li>Energy Efficient Furnaces</li>
+                                    <li>High-Efficiency Heat Pumps</li>
+                                    <li>Heating and Furnace Maintenance</li>
+                                    <li>New Gas Furnace Installation</li>
+                                    <li>HVAC Repairs</li>
+                                    <li>Duct Cleaning</li>
+                                    <li>Whole Home Humidifiers</li>
+                                    <li>Central Air Repair</li>
+                                    <li>WiFi Thermostats</li>
+                                    <li>Lennox Dealer</li>
+                                    <li>NATE Certification Techs</li>
+                                    <li>HVAC Replacement</li>
+                                    <li>Smart Thermostats</li>
+                                </ul>
+
+
+                                <h2>Paris, Illinois HVAC Installations</h2>
+
+                                <p>Upgrade your home's comfort system with a new Lennox high-efficiency heating and
+                                    cooling system from Paitson Bros. Heating and Air Conditioning in Paris. Enjoy
+                                    substantial energy savings and year-round comfort. Our Paris HVAC solutions span
+                                    various budgets, and we specialize in advanced technology and smart thermostat
+                                    control for effortless system management. Trust our expert Paris-based team for
+                                    top-notch HVAC installations.</p>
+
+                                <h2>Whole Home Generators, Paris, IL</h2>
+
+                                <p>Discover unmatched convenience with Whole Home Generators installed in Paris,
+                                    Illinois by Paitson Bros. As your trusted Generac PowerPro dealer, we offer decades
+                                    of experience ensuring reliable energy solutions for your home or business,
+                                    especially during unpredictable weather events. Explore seamless installation and
+                                    cost-effective options. Trust Paitson Bros. for efficient backup power solutions in
+                                    Paris, backed by our expertise as a Generac PowerPro dealer. <a
+                                        href="<?php echo $base_url ?>/generators.php">Learn
+                                        More</a></p>
+
+                                <p><strong>Paitson Bros. Heating and Air Conditioning: A Century of Comfort, A Legacy of
+                                        Excellence in Paris, Illinois</strong></p>
+
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Contact
+                                    Us</a>
+                                <br><br>
+
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('../footer.php'); ?>
