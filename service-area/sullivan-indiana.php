139aa6b1c1b44f9f0b05569cc6929dddb4f3216f
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-01-09 12:12:14 +0500

    sulivan updated

---
+++ service-area/sullivan-indiana.php
@@ -0,0 +1,157 @@
+<?php include_once('../header.php') ?>
+<title>Furnace Repair Sullivan, IN | AC Repair| HVAC Sullivan, IN| Paitson Bros. </title>
+<meta name="description"
+    content=" Sullivan Indiana's Premier HVAC and Furnace Repair Experts: Paitson Bros. Heating and Air Conditioning.  A Century of Comfort, A Legacy of Excellence " />
+
+<meta name="rating" content="general" />
+<meta name="revisit-after" content="3 days" />
+<meta name="robots" content="index, follow" />
+<meta name="author" content="CG" />
+
+<?php include_once('../menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <h1>Sullivan, Indiana</h1>
+
+
+                                <img src="<?php echo $base_url ?>/img/Sullivan Indiana (3).jpeg" alt="Sullivan indiana"
+                                    style=" width: 100%; padding: 20px;" class="duct-clean-img">
+                                <br>
+
+                                <p>Tucked away in the Southwest corner of Indiana surrounded by beautiful lakes and
+                                    farms, lies the historic and charming community of Sullivan, Indiana. The Sullivan
+                                    Courthouse built in the early 1920's has achieved a designation on the National
+                                    Register of Historic Places. Every September and for over 60 years the Sullivan
+                                    Annual Rotary Corn Festival has been held on the City Square and features a parade,
+                                    carnival, live music vendors, food, and other fun activities. Sullivan is the host
+                                    of many other wonderful events all year long. Sullivan is a place where rich history
+                                    and friendly residents blend to create a wonderful community.
+                                </p>
+
+                                <h2>Sullivan Indiana's Premier HVAC and Furnace Repair Experts: Paitson Bros. Heating
+                                    and Air Conditioning - A Century of Comfort, A Legacy of Excellence</h2>
+
+                                <p>Discover superior HVAC services right here in Sullivan, Indiana with Paitson Bros.
+                                    Heating and Air Conditioning. For over 100 years, we've been the trusted choice for
+                                    homeowners in Sullivan and its neighboring regions. Whether you need furnace
+                                    repairs, installations, or top-tier air conditioning services, our dedicated team is
+                                    committed to ensuring your home stays comfortable. Count on us for 24/7 emergency
+                                    services and comprehensive HVAC solutions tailored to your needs.</p>
+
+                                <h2>Air Conditioning Services Sullivan, IN</h2>
+
+                                <p>Stay cool in Sullivan, Indiana, with Paitson Bros. Heating and Air Conditioning! Our
+                                    all-encompassing air conditioning services cater to your every need, whether it's
+                                    repairs, replacements, recharging, or maintenance for both residential and
+                                    commercial properties in Sullivan. Count on us as your reliable 24-hour emergency
+                                    HVAC service provider. We take pride in our expertise with all major brands of home
+                                    cooling equipment, ensuring top-notch service and performance every time. Explore
+                                    our value-packed maintenance plans for great benefits at an affordable price. <a
+                                        href="<?php echo $base_url ?>/maintenance-plan.php">Learn
+                                        more</a></p>
+
+                                <h3>Air Conditioning Services Sullivan, IN</h3>
+
+                                <ul>
+                                    <li>Energy-Efficient Central Air Systems</li>
+                                    <li>Cooling Repair</li>
+                                    <li>Commercial HVAC</li>
+                                    <li>AC Replacements</li>
+                                    <li>Air Conditioning Repair</li>
+                                    <li>AC Repair</li>
+                                    <li>New Home HVAC Installation</li>
+                                    <li>New AC Installation</li>
+                                    <li>High Efficiency HVAC</li>
+                                    <li>Central Air Repair</li>
+                                    <li>AC Tune-Ups</li>
+                                    <li>High-Efficiency AC</li>
+                                    <li>24/7 Emergency Services</li>
+                                    <li>Mini Splits</li>
+                                    <li>Smart Thermostats</li>
+                                </ul>
+
+                                <h2>Furnace Repair Services in Sullivan, Indiana</h2>
+
+                                <p>Count on Paitson Bros. Heating and Air Conditioning for comprehensive furnace repair
+                                    services in Sullivan. Whether you need a new furnace installation, replacement, or
+                                    swift repairs for your Sullivan residence, our dedicated team is committed to
+                                    ensuring your home stays warm and cozy. We take pride in offering 24/7 emergency
+                                    furnace repair services and possess expertise in servicing all brands of home
+                                    heating equipment. Rest assured, your furnace is in capable hands with us.</p>
+
+                                <h3>Furnace Services Sullivan, Indiana</h3>
+
+                                <ul>
+                                    <li>Furnace Repair</li>
+                                    <li>Heating Repair</li>
+                                    <li>High Efficiency Furnaces</li>
+                                    <li>24/7 Emergency Furnace Repair</li>
+                                    <li>New Furnace Installation</li>
+                                    <li>Heat Pumps</li>
+                                    <li>Duel Fuel Systems</li>
+                                    <li>Energy Efficient Furnaces</li>
+                                    <li>High-Efficiency Heat Pumps</li>
+                                    <li>Heating and Furnace Maintenance</li>
+                                    <li>New Gas Furnace Installation</li>
+                                    <li>HVAC Repairs</li>
+                                    <li>Duct Cleaning</li>
+                                    <li>Whole Home Humidifiers</li>
+                                    <li>Central Air Repair</li>
+                                    <li>WiFi Thermostats</li>
+                                    <li>Lennox Dealer</li>
+                                    <li>NATE Certification Techs</li>
+                                    <li>HVAC Replacement</li>
+                                    <li>Smart Thermostats</li>
+                                </ul>
+
+                                <h2>Sullivan, Indiana HVAC Installations</h2>
+
+                                <p>Upgrade your home's comfort system with a new Lennox high-efficiency heating and
+                                    cooling system from Paitson Bros. Heating and Air Conditioning in Sullivan. Enjoy
+                                    substantial energy savings and year-round comfort. Our Sullivan HVAC solutions span
+                                    various budgets, and we specialize in advanced technology and smart thermostat
+                                    control for effortless system management. Trust our expert Sullivan-based team for
+                                    top-notch HVAC installations.</p>
+
+                                <h2>Whole Home Generators, Sullivan, IN</h2>
+
+                                <p>Discover unmatched convenience with Whole Home Generators installed in Sullivan,
+                                    Indiana by Paitson Bros. As your trusted Generac PowerPro dealer, we offer decades
+                                    of experience ensuring reliable energy solutions for your home or business,
+                                    especially during unpredictable weather events. Explore seamless installation and
+                                    cost-effective options. Trust Paitson Bros. for efficient power solutions in
+                                    Sullivan, backed by our expertise as a Generac PowerPro dealer. <a
+                                        href="<?php echo $base_url ?>/generators.php">Learn
+                                        More</a></p>
+
+                                <p><strong>Paitson Bros. Heating and Air Conditioning: A Century of Comfort, A Legacy of
+                                        Excellence in Sullivan</strong></p>
+
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Contact
+                                    Us</a>
+                                <br><br>
+
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('../footer.php'); ?>
