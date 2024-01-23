cb8af3cab3068dcd1ed2d84658d02b1c8c3ffab5
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-01-23 14:57:14 +0500

    Update specials.php

--- specials.php
+++ specials.php
@@ -65,7 +65,8 @@
 
                                 <p><strong>Call Right Now</strong></p>
 
-                                <p>812-232-2347 or toll-free 1-800-533-0711</p>
+                                <p><a href="tel:+18122322347">812-232-2347</a> or toll-free <a
+                                        href="tel:+18005330711">1-800-533-0711</a></p>
 
                                 <p><strong>Paitson Bros. Heating and Air Conditioning: A Century of Comfort, A Legacy of
                                         Excellence</strong></p>
