8f290cdd42ee0c9d0e66563d3e2c6851560fadd8
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-01-23 11:30:44 +0500

    special page updated

---
+++ specials.php
@@ -0,0 +1,70 @@
+<?php include_once('header.php') ?>
+
+<title>Paitson Bros. Heating and Air Conditioning | Specials</title>
+
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <h1>Specials</h1>
+                                <img src="<?php echo $base_url ?>/img/Lennox_salebanner.png"
+                                    alt="What's on Sale banner">
+                                <br>
+                            </div>
+
+                            <div class="col-md-6">
+                                <img src="<?php echo $base_url ?>/img/1 (1).png" alt="AC installed ">
+                                <p>* Some Restrictions apply, contact us for more information</p>
+                            </div>
+                            <div class="col-md-6">
+                                <img src="<?php echo $base_url ?>/img/2 (1).png" alt="Furnace Installed">
+                                <p>** Some Restrictions apply, contact us for more information</p>
+                            </div>
+                            <div class="col-md-6">
+                                <img src="<?php echo $base_url ?>/img/3 (1).png" alt="Furnace and AC Installed">
+                            </div>
+
+                            <div class="col-sm-12">
+                                <p>*** Offer applies only to single-receipt qualifying purchases.
+                                    Some Restrictions apply, contact us for more information
+                                    Monthly interest will be assessed on promo purchases from the purchase date at a
+                                    reduced
+                                    7.99% APR for 120 months, and fixed monthly payments are required equal to 1.21% of
+                                    the
+                                    initial promo purchase amount until the promo is paid in full. Subject to credit
+                                    approval.</p>
+
+                                <p><strong>For a Free No Obligation In-Home Estimate</strong></p>
+
+                                ---Schedule Now Button---
+
+                                <p><strong>Or</strong></p>
+
+                                <p><strong>Call Right Now</strong></p>
+
+                                <p>812-232-2347 or toll-free 1-800-533-0711</p>
+
+                                <p><strong>Paitson Bros. Heating and Air Conditioning: A Century of Comfort, A Legacy of
+                                        Excellence</strong></p>
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
