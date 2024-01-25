3548f5cd2f5d0f508ea3fd648b6b6d0c12af6551
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-01-25 17:23:55 +0500

    commericial page updated

---
+++ commercial.php
@@ -0,0 +1,113 @@
+<?php include_once('header.php') ?>
+<title>Paitson Bros. Heating and Air Conditioning| Commercial HVAC </title>
+
+<meta name="description"
+    content="Paitson Bros. Heating and Air Conditioning has been providing commercial installations of the newest and most energy-efficient HVAC systems available, routine maintenance of HVAC systems, and fast expert HVAC repair, for over 100 years. " />
+
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-10 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+
+                                <img src="<?php echo $base_url ?>/img/comerial-banner (1).png"
+                                    alt="Commercial HVAC page banner" class="full-width-img">
+                                <br>
+                                <p>Welcome to Paitson Bros. Heating and Air Conditioning, your premier partner for
+                                    Commercial HVAC solutions. Our mission is to redefine comfort in your business
+                                    space,
+                                    catering to both employees and customers. Boasting over a century of expertise, we
+                                    are
+                                    proud to offer state-of-the-art HVAC systems that not only guarantee a comfortable
+                                    indoor environment but also deliver substantial savings on your utility costs.</p>
+
+                                <h2>Why Choose Paitson Bros.?</h2>
+
+                                <p>Our unwavering commitment to customer satisfaction distinguishes us. From the
+                                    installation of cutting-edge, energy-efficient Commercial HVAC systems to routine
+                                    maintenance and swift, expert repairs, we prioritize excellence in every service.
+                                </p>
+
+                                <h2>Tailored HVAC Services for Your Business:</h2>
+
+                                <p>Paitson Bros. specializes in custom-tailored HVAC solutions, meticulously crafted to
+                                    meet your specific needs and budget requirements. Our comprehensive services
+                                    encompass the installation, maintenance, and repair of HVAC systems, furnaces, air
+                                    conditioning units, packaged units, and rooftop units. We excel in servicing and
+                                    repairing all brands and makes of commercial heating and air conditioning equipment.
+                                </p>
+
+                                <h2>Unlock the Paitson Bros. Advantage:</h2>
+
+                                <p>Explore the benefits of our holistic preventative maintenance agreements, designed to
+                                    offer peace of mind while helping you steer clear of unnecessary service calls and
+                                    save significantly on energy costs. Entrust your HVAC equipment to Paitson Bros.
+                                    Heating and Air Conditioning, your reliable mechanical contractor.</p>
+
+                                <h2>Elevate Your Comfort with Paitson Bros.:</h2>
+
+                                <p>Choose the power of "Commercial HVAC" expertise with Paitson Bros. Join us in
+                                    maintaining a legacy of over a century, dedicated to keeping businesses comfortably
+                                    cool or warm. Contact us today to discuss your Commercial
+                                    HVAC needs and experience a new level of indoor comfort.</p>
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Contact Us</a>
+                                <br><br>
+
+                                <h2>Paitson Bros. Heating and Air Conditioning, Commercial HVAC Services:</h2>
+
+                                <ul>
+                                    <li>Commercial HVAC Repair</li>
+                                    <li>Commercial Heating/AC Installation</li>
+                                    <li>Ductless Air Conditioning Split Systems</li>
+                                    <li>All-in-One Solution Packaged Systems</li>
+                                    <li>Complete Seasonal Inspections of HVAC systems</li>
+                                    <li>Emergency Repair Service</li>
+                                    <li>VRF Systems</li>
+                                    <li>Temperature/Humidity Control</li>
+                                    <li>Commercial HVAC Maintenance Plans</li>
+                                    <li>Indoor Air Quality</li>
+                                    <li>HVAC Sales, Repair, Installation</li>
+                                    <li>Rooftop Units</li>
+                                    <li>HVAC Duct Systems and Modifications</li>
+                                </ul>
+                                <br>
+                                <p> <strong>Paitson Bros. Heating and Air Conditioning: A Century of Comfort, A Legacy
+                                        of
+                                        Excellence</strong></p>
+
+
+
+                                <div class="video-container">
+                                    <iframe src="https://player.vimeo.com/video/904470814?h=54a9f57f7a" width="450"
+                                        height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture"
+                                        allowfullscreen></iframe>
+
+                                </div>
+
+
+                                <br>
+
+                                <br>
+
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
