a53c73aca9069dd6019f8d991f98253d6747d13f
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-01-25 17:04:57 +0500

    hvac video updated

--- hvac-construction.php
+++ hvac-construction.php
@@ -95,13 +95,11 @@
 
 
 
-                                <iframe src="https://player.vimeo.com/video/904507083?h=bf3c552e47" width="640"
-                                    height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture"
-                                    allowfullscreen></iframe>
-                                <p><a href="https://vimeo.com/904507083">HVAC Servces for New Home Construction</a> from
-                                    <a href="https://vimeo.com/user207156410">Ethan</a> on <a
-                                        href="https://vimeo.com">Vimeo</a>.
-                                </p>
+                                <div class="video-container">
+                                    <iframe src="https://player.vimeo.com/video/904507083?h=bf3c552e47" frameborder="0"
+                                        allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+                                </div>
+
 
                                 <br>
 
