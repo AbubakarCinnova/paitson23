1d83c10fad640e7be05dcfd804e508195bb0d806
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2024-04-04 10:55:06 +0500

    heat-pumps page updated

--- heat-pumps.php
+++ heat-pumps.php
@@ -1,6 +1,7 @@
 <?php include_once('header.php') ?>
 
 <title>Paitson Bros. Heating and Air Conditioning| Heat Pumps</title>
+
 <meta name="description"
     content=" When it comes to getting the best high-efficiency heat pump or heat pump repair in Terre Haute, the only name you need to know is Paitson Bros. Heating and Air Conditioning.">
 <meta name="rating" content="general" />
@@ -22,100 +23,103 @@
                         <div class="row">
                             <div class="col-sm-12">
 
-                                <h1>Heat Pumps: A Comprehensive Solution for Year-Round Comfort in Indiana Homes</h1>
+                                <img src="<?php echo $base_url ?>/img/duelfuel_1000X350 (1).png"
+                                    alt="pic Stanley Steemer" style=" width: 100%; padding-top: 20px;"
+                                    class="duct-clean-img">
+                                <br>
 
-                                <p>In Indiana, homeowners are discovering the exceptional versatility of heat pumps,
-                                    which function seamlessly as both heating and cooling systems. Combining a heat pump
-                                    with a furnace creates a dual-fuel system, ensuring year-round comfort. As
-                                    temperatures rise, the heat pump operates as an efficient air conditioner during the
-                                    summer, while in colder months, it adeptly extracts heat from the outdoor air to
-                                    keep homes warm and cozy.</p>
+                                <h2>The Dual-Fuel Versatility of Heat Pumps in Indiana Homes</h2>
 
-                                <p>To optimize performance, the heat pump operates most effectively when temperatures
-                                    exceed 35 degrees Fahrenheit. As temperatures drop further, the furnace (whether
-                                    gas, electric, or oil) takes on the role of the primary heating source. In locales
-                                    like Indiana, where temperature dips can be substantial, homeowners may seek an
-                                    additional heating solution. By combining a Lennox heat pump with a highly efficient
-                                    furnace powered by gas, electricity, or oil, an energy-saving dual-fuel system is
-                                    established. This approach not only reduces reliance on a single energy source but
-                                    also ensures optimal comfort even on the coldest days.</p>
+                                <p>Discover the growing preference for heat pumps among Indiana homeowners, thanks to
+                                    their dual functionality as heating and cooling systems. When an energy-efficient
+                                    heat pump is combined with an energy-efficient furnace, it forms a versatile
+                                    dual-fuel system, ensuring year-round comfort. In warmer months, the heat pump
+                                    efficiently cools the air, while in colder months, it extracts heat from the outdoor
+                                    air to keep homes warm.</p>
 
-                                <p>The efficiency of our system is shaped by various factors, including HVAC system
-                                    design, size, and maintenance, as well as local climate, building insulation, and
-                                    occupant behavior. At Paitson Bros., our team of comfort designers specializes in
-                                    thorough analyses and load calculations. We help you select the ideal HVAC system
-                                    for residential and commercial properties. By meticulously considering these crucial
-                                    elements, we assure you of heightened energy efficiency and comfort for your home or
-                                    business.</p>
+                                <p>Optimal performance is achieved when the heat pump operates above 35 degrees
+                                    Fahrenheit, with the furnace stepping in as the primary heating source as
+                                    temperatures drop further. By integrating a Lennox energy-efficient heat pump with a
+                                    high-efficiency furnace, homeowners can establish an energy-saving dual-fuel system,
+                                    reducing reliance on a single energy source and maintaining comfort even on the
+                                    coldest days.</p>
 
-                                <p> <strong>The amended Energy Efficient Home Improvement Credit, which began in 2023
-                                        and extends
+                                <p>Paitson Bros. offers expert analysis and load calculations to assist homeowners in
+                                    selecting the ideal HVAC system, ensuring heightened energy efficiency and comfort.
+                                </p>
+
+                                <p><strong>The amended Energy Efficient Home Improvement Credit, which began in 2023 and
+                                        extends
                                         through 2032, increases the tax credits as high as $600 for qualified air
-                                        conditioners or gas furnaces, and up to $2000 for qualified heat pumps, heat
-                                        pump
-                                        water heaters, or boilers.</strong></p>
-
-                                <p>Paitson Bros. Heating and Air Conditioning is dedicated to assisting customers in
-                                    maximizing their credits for new heat pump installations. By leveraging the Federal
-                                    Inflation Reduction Act alongside local Utility Rebates and manufacturer incentives,
-                                    we actively work with our customers to ensure they receive the highest possible
-                                    credits for their purchases.</p>
+                                        conditioners or gas furnaces, and up to $2000 for qualified heat pumps.</strong>
+                                </p>
 
                                 <h2>Lennox Model SL25XPV HEAT PUMP</h2>
-                                <p>The most precise and efficient heat pump you can buy. Now featuring Lennox
-                                    TruHeat Performance.</p>
+                                <p>The most precise and efficient heat pump you can buy. Now featuring Lennox TruHeat
+                                    Performance.</p>
                                 <ul>
                                     <li>Stages of Cooling: Variable-Capacity Operation</li>
-                                    <li>Energy Efficiency 24</li>
+                                    <li>Energy Efficiency: 24</li>
                                     <li>ENERGY STAR®</li>
-                                    <li>Sound Level As low as 58 dBA</li>
+                                    <li>Sound Level: As low as 58 dBA</li>
                                     <li>SilentComfort Technology</li>
-                                    <li>Cabinet PermaGuard Cabinet and SmartHinge Louver Design</li>
+                                    <li>Cabinet: PermaGuard Cabinet and SmartHinge Louver Design</li>
                                     <li>R-410A refrigerant</li>
                                     <li>Tax Credit Eligible: Yes</li>
                                     <li>iComfort-enabled technology</li>
                                     <li>All Dave Lennox Signature® Collection models come with a 10-Year Limited
-                                        Warranty on the compressor and covered components.</li>
+                                        Warranty on
+                                        the compressor and covered components.</li>
                                 </ul>
 
-                                <h2>Lennox Model EL22XPV HEAT PUMP</h2>
-                                <p>High-efficiency, digitally-enabled, variable-capacity heat pump</p>
+                                <h2>Lennox Model EL18XPV HEAT PUMP</h2>
+                                <p>Mid-efficiency, digitally-enabled, variable-capacity heat pump</p>
                                 <ul>
                                     <li>Stages of Cooling: Variable-Capacity Operation</li>
-                                    <li>22.00 SEER2 and 9.50 HSPF2</li>
+                                    <li>SEER: 218</li>
+                                    <li>HSPF: 10.00</li>
                                     <li>ENERGY STAR®</li>
-                                    <li>Sound Level As low as 61 dBA</li>
-                                    <li>Cabinet PermaGuard Cabinet and SmartHinge Louver Design</li>
+                                    <li>Sound Level: As low as 61 dBA</li>
+                                    <li>Cabinet: PermaGuard Cabinet and SmartHinge Louver Design</li>
                                     <li>R-410A refrigerant</li>
                                     <li>Tax Credit Eligible: Yes</li>
                                     <li>Digitally-enabled technology</li>
-                                    <li>10-Year Limited Warranty on compressor, 5-Year Limited Warranty on covered
-                                        components. This product may be eligible for valuable 10-Year Limited Extended
-                                        Warranty coverage with product registration.</li>
+                                    <li>Elite models come with a 10-Year Limited Warranty on compressors and a 5-Year
+                                        Limited
+                                        Warranty on covered components. A 10-Year Extended Limited Warranty is available
+                                        with product registration.</li>
                                 </ul>
 
-                                <h2>Lennox Model EL16XP1 HEAT PUMP</h2>
-                                <p>A highly efficient, single-stage heat pump that delivers exceptional comfort</p>
+                                <h2>Lennox Model EL17XP1 HEAT PUMP</h2>
+                                <p>Mid-efficiency, single-stage heat pump</p>
                                 <ul>
                                     <li>Stages of Cooling: Single Stage</li>
-                                    <li>Up to 17 (SEER), Up to 9.8 (HSPF)</li>
+                                    <li>SEER: 18.60</li>
+                                    <li>HSPF: 10.60</li>
                                     <li>ENERGY STAR®</li>
-                                    <li>Sound Level As low as 71 dBA</li>
-                                    <li>Cabinet PermaGuard Cabinet</li>
+                                    <li>Sound Level: As low as 71 dBA</li>
+                                    <li>Cabinet: PermaGuard Cabinet</li>
                                     <li>R-410A refrigerant</li>
-                                    <li>Tax Credit Eligible: Yes</li>
                                     <li>Elite models come with a 10-Year Limited Warranty on compressors and a 5-Year
-                                        Limited Warranty on covered components. A 10-Year Extended Limited Warranty is
-                                        available with product registration.</li>
+                                        Limited
+                                        Warranty on covered components. A 10-Year Extended Limited Warranty is available
+                                        with product registration.</li>
                                 </ul>
+
+
+                                <br>
+
+
                                 <br>
                                 <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Request
                                     Quote</a>
                                 <br><br>
-                                <p><strong>When it comes to getting the best high-efficiency heat pump or heat pump
-                                        repair in
-                                        Terre Haute, the only name you need to know is Paitson Bros. Heating and Air
+
+                                <p><strong>
+                                        When getting the best high-efficiency heat pump or heat pump repair in Terre
+                                        Haute, the only name you need to know is Paitson Bros. Heating and Air
                                         Conditioning.</strong></p>
+
 
 
 
